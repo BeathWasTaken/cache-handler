e84f567145b49db5dcfacb54ff5693123d48161e
Beath Here <oldbeath@gmail.com>
2026-06-06 18:15:56 +0700

    test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,28 @@
+const path = require("path");
+const fs = require("fs");
+
 module.exports = (req, res) => {
-    res.status(200).send("Hello World");
+    const url = req.url.split("?")[0];
+
+    if (url.startsWith("/cache/")) {
+        const fileName = url.replace("/cache/", "");
+
+        const filePath = path.join(
+            process.cwd(),
+            "public",
+            "cache",
+            fileName
+        );
+
+        if (!fs.existsSync(filePath)) {
+            return res.status(404).send(`File not found: ${filePath}`);
+        }
+
+        const buffer = fs.readFileSync(filePath);
+
+        res.setHeader("Content-Type", "application/octet-stream");
+        return res.status(200).send(buffer);
+    }
+
+    return res.status(404).send("Not found");
 };
